615375e40bb98fae9cbb8f67c9afbb2d2ed291ac
Eber Omar Garza Fuentes <a24110207@live.ceti.mx>
2025-03-25 23:03:24 +0000

    Version 0

---
+++ Foco.hpp
@@ -0,0 +1,24 @@
+class Foco
+{
+private:
+ bool estadoActual;
+
+public:
+    Foco() {}
+
+    ~Foco() {}
+
+void Encender(){
+    estadoActual = true;
+}
+
+void Apagar(){
+    estadoActual = false;
+}
+
+bool LeerEstado (){
+
+return estadoActual;
+
+}
+};
